53dc2f7d72bf4a3669f3056edb92928cff45a7c4
azrail212 <azra.becirovic@stu.ibu.edu.ba>
2022-05-14 00:34:46 +0200

    mock login without styling

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginComponent from "../views/LoginView.vue"
+import SecureComponent from "../views/SecureView.vue"
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            redirect: {
+                name: "login"
+            }
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: LoginComponent
+        },
+        {
+            path: "/secure",
+            name: "secure",
+            component: SecureComponent
+        }
+    ]
 })
-
-export default router
